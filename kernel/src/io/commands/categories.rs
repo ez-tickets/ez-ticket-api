c52c004ea91e86d58c270c0db1d6532dc0182423
ReiRokusanami0010 <reirokusanami.rdh@gmail.com>
2025-01-18 23:32:57 +0900

    :hammer: - Added processing so that `CategoriesCommand` can be generated from `CategoryEvent`.

--- kernel/src/io/commands/categories.rs
+++ kernel/src/io/commands/categories.rs
@@ -1,6 +1,10 @@
-use crate::entities::category::CategoryId;
-use nitinol::macros::Command;
 use std::collections::BTreeMap;
+use error_stack::Report;
+use nitinol::macros::Command;
+
+use crate::entities::category::CategoryId;
+use crate::errors::FormationError;
+use crate::io::events::CategoryEvent;
 
 /// CategoriesCommand is a command that can be applied to a [`Categories`](crate::entities::categories::Categories) entity.
 ///
@@ -14,5 +18,16 @@ use std::collections::BTreeMap;
 pub enum CategoriesCommand {
     AddCategory { id: CategoryId },
     RemoveCategory { id: CategoryId },
-    ChangeOrdering { new: BTreeMap<i32, CategoryId> },
+    ChangeOrdering { new: BTreeMap<i64, CategoryId> },
+}
+
+impl TryFrom<CategoryEvent> for CategoriesCommand {
+    type Error = Report<FormationError>;
+    fn try_from(value: CategoryEvent) -> Result<Self, Self::Error> {
+        match value {
+            CategoryEvent::Created { id, .. } => Ok(Self::AddCategory { id }),
+            CategoryEvent::Deleted { id } => Ok(Self::RemoveCategory { id }),
+            _ => Err(Report::new(FormationError).attach_printable("Cannot convert event to command."))?,
+        }
+    }
 }
